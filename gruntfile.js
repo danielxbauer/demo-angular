20ad8e33428381eeff2e2eff0a9094875a54acad
pangeax <daniel.bauer.x@gmail.com>
2016-09-14 01:37:15 +0200

    Grunt Prod Task
    coping index template html and injecting is not working in prod
    currently

--- gruntfile.js
+++ gruntfile.js
@@ -7,40 +7,41 @@
     grunt.loadNpmTasks('grunt-include-source');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    var vendors = [
-        'jquery/dist/jquery.min.js',
-        'angular/angular.min.js',
-        'angular-ui-router/release/angular-ui-router.js',
-        'bootstrap-sass/assets/javascripts/bootstrap.js'
-    ];
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    var vendors = grunt.file.readJSON("app/vendors.json");
+
     var vendorsInLib = vendors.map(function (path) { return 'wwwroot/libs/' + path; });
+    var vendorsJs = vendorsInLib.filter(function(path) { return path.endsWith(".js"); });
+    var vendorsCss = vendorsInLib.filter(function(path) { return path.endsWith(".css"); });
 
     grunt.initConfig({
         config: {
             dev: "wwwroot",
-            prod: ""
+            prod: "wwwrootProd" // TODO: this is just to test the prod tasks
         },
         vendors: vendorsInLib,
+        vendorsJs: vendorsJs,
+        vendorsCss: vendorsCss,
 
         clean: {
-            dev: [
-                '<%= config.dev %>/js',
-                '<%= config.dev %>/css',
-                '<%= config.dev %>/views',
-                '<%= config.dev %>/libs',
-                '<%= config.dev %>/index.html'
-            ]
-        },
-        copy: {
-            dev: {
-                files: [
-                    /* copies views to wwwroot */
-                    { expand: true, cwd: 'app/', src: ['**/*.html', '!index.tpl.html'], dest: '<%= config.dev %>/views', filter: 'isFile' },
+            dev: '<%= config.dev %>',
 
-                    /* copies bower_components to wwwroot/libs */
-                    { expand: true, cwd: 'bower_components/', src: vendors, dest: '<%= config.dev %>/libs', filter: 'isFile' }
-                ]
-            }
+            devJs: '<%= config.dev %>/js',
+            devCss: '<%= config.dev %>/css',
+
+            prod: '<%= config.prod =>'
+        },
+        copy: {            
+            /* copies bower_components to wwwroot/libs */
+            devVendor: { expand: true, cwd: 'bower_components/', src: vendors, dest: '<%= config.dev %>/libs', filter: 'isFile' },
+            
+            /* copies views to wwwroot */
+            devViews: { expand: true, cwd: 'app/', src: ['**/*.html', '!index.tpl.html'], dest: '<%= config.dev %>/views', filter: 'isFile' },
+
+            /* copies views from wwwroot to prod */
+            prodViews: { expand: true, cwd: '<%= config.dev %>/views', src: ['**/*.html'], dest: '<%= config.prod %>/views', filter: 'isFile' }
         },
         typescript: {
             dev: {
@@ -65,33 +66,60 @@
             }
         },
         includeSource: {
-            options: {
-            },
             dev: {
                 files: {
                     '<%= config.dev %>/index.html': 'app/index.tpl.html'
                 }
+            },
+            prod: {
+                files: {
+                    '<%= config.prod %>/index.html': 'app/index.tpl.html'
+                }
             }
         },
         watch: {
-            js: {
+            app: {
                 files: ['app/**/*.ts'],
-                tasks: ['typescript:dev', 'includeSource:dev']
+                tasks: ['clean:devJs', 'typescript:dev', 'includeSource:dev']
             },
             css: {
                 files: ['sass/**/*.scss'],
-                tasks: ['sass:dev']
+                tasks: ['clean:devCss', 'sass:dev', 'includeSource:dev']
             },
             index: {
                 files: ['app/index.tpl.html'],
-                tasks: ['includeSource:dev', 'wiredep:dev']
+                tasks: ['includeSource:dev']
             },
             views: {
                 files: ['app/**/*.html', '!app/index.tpl.html'],
-                tasks: ['copy']
+                tasks: ['copy:devViews']
             }
         },
+
+        uglify: {
+            prod: {
+                files: {
+                    '<%= config.prod %>/app.min.js': '<%= config.dev %>/js/**/*.js',
+                    '<%= config.prod %>/vendors.min.js': '<%= vendorsJs %>',
+                }
+            }
+        },
+        cssmin: {
+            prod: {
+                files: {
+                    '<%= config.prod %>/app.min.css': '<%= config.dev %>/css/**/*.css',
+                    '<%= config.prod %>/vendor.min.css': '<%= vendorsCss %>'
+                }
+            }
+        }
     });
 
-    grunt.registerTask('dev', ['clean:dev', 'copy:dev', 'typescript:dev', 'sass:dev', 'includeSource:dev']); //, 'watch']);
+    grunt.registerTask('dev', ['devWithoutWatch', 'watch']);
+    grunt.registerTask('devWithoutWatch', ['clean:dev', 'copy:devVendor', 'copy:devViews', 'typescript:dev', 'sass:dev', 'includeSource:dev']);
+
+    grunt.registerTask('prod', ['clean:prod', 'devWithoutWatch', 'copy:prodViews', 'uglify:prod', 'cssmin:prod', 'daniel', 'includeSource:prod']);
+
+    grunt.registerTask("daniel", "", function () {
+        grunt.config("vendors", [''])
+    });
 };
