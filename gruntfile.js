a151271911bbb7682e8e312c4510c2c9cb9197ed
pangeax <daniel.bauer.x@gmail.com>
2016-09-14 20:37:49 +0200

    fully functional grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,32 @@
 ﻿module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-typescript');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-include-source');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');      // to delete folder, files
+    grunt.loadNpmTasks('grunt-contrib-copy');       // to copy folder, files
+    grunt.loadNpmTasks('grunt-typescript');         // to compile typescript to js
+    grunt.loadNpmTasks('grunt-sass');               // to compile sass to css
+    grunt.loadNpmTasks('grunt-include-source');     // to include js, css tags into index.html
+    grunt.loadNpmTasks('grunt-contrib-watch');      // to watch files and do tasks
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-
-    var vendors = grunt.file.readJSON("app/vendors.json");
-
-    var vendorsInLib = vendors.map(function (path) { return 'wwwroot/libs/' + path; });
-    var vendorsJs = vendorsInLib.filter(function(path) { return path.endsWith(".js"); });
-    var vendorsCss = vendorsInLib.filter(function(path) { return path.endsWith(".css"); });
+    grunt.loadNpmTasks('grunt-contrib-uglify');     // to minify js
+    grunt.loadNpmTasks('grunt-contrib-cssmin');     // to minify css
 
     grunt.initConfig({
         config: {
             dev: "wwwroot",
-            prod: "wwwrootProd" // TODO: this is just to test the prod tasks
+            prod: "wwwrootprod"
         },
-        vendors: vendorsInLib,
-        vendorsJs: vendorsJs,
-        vendorsCss: vendorsCss,
 
         clean: {
             dev: '<%= config.dev %>',
+            prod: '<%= config.prod %>',
 
+            /* above ones for watch task */
             devJs: '<%= config.dev %>/js',
             devCss: '<%= config.dev %>/css',
-
-            prod: '<%= config.prod =>'
         },
         copy: {            
             /* copies bower_components to wwwroot/libs */
-            devVendor: { expand: true, cwd: 'bower_components/', src: vendors, dest: '<%= config.dev %>/libs', filter: 'isFile' },
+            devVendor: { expand: true, cwd: 'bower_components/', src: '<%= vendors %>', dest: '<%= config.dev %>/libs', filter: 'isFile' },
             
             /* copies views to wwwroot */
             devViews: { expand: true, cwd: 'app/', src: ['**/*.html', '!index.tpl.html'], dest: '<%= config.dev %>/views', filter: 'isFile' },
@@ -44,15 +35,15 @@
             prodViews: { expand: true, cwd: '<%= config.dev %>/views', src: ['**/*.html'], dest: '<%= config.prod %>/views', filter: 'isFile' }
         },
         typescript: {
+            options: {
+                module: 'amd', //or commonjs 
+                target: 'es5', //or es3
+                sourceMap: true,
+                declaration: true
+            },
             dev: {
                 src: ['app/**/*.ts'],
-                dest: '<%= config.dev %>/js',
-                options: {
-                    module: 'amd', //or commonjs 
-                    target: 'es5', //or es3
-                    sourceMap: true,
-                    declaration: true
-                }
+                dest: '<%= config.dev %>/js'                
             }
         },
         sass: {
@@ -96,11 +87,12 @@
             }
         },
 
+        /* uglify and cssmin are only used in prod task */
         uglify: {
             prod: {
                 files: {
                     '<%= config.prod %>/app.min.js': '<%= config.dev %>/js/**/*.js',
-                    '<%= config.prod %>/vendors.min.js': '<%= vendorsJs %>',
+                    '<%= config.prod %>/vendors.min.js': '<%= vendorsDevJs %>',
                 }
             }
         },
@@ -108,18 +100,43 @@
             prod: {
                 files: {
                     '<%= config.prod %>/app.min.css': '<%= config.dev %>/css/**/*.css',
-                    '<%= config.prod %>/vendor.min.css': '<%= vendorsCss %>'
+                    '<%= config.prod %>/vendor.min.css': '<%= vendorsDevCss %>'
                 }
             }
         }
     });
 
     grunt.registerTask('dev', ['devWithoutWatch', 'watch']);
-    grunt.registerTask('devWithoutWatch', ['clean:dev', 'copy:devVendor', 'copy:devViews', 'typescript:dev', 'sass:dev', 'includeSource:dev']);
+    grunt.registerTask('devWithoutWatch', ['clean:dev', 'setupVarsDev', 'copy:devVendor', 'copy:devViews', 'typescript:dev', 'sass:dev', 'includeSource:dev']);
 
-    grunt.registerTask('prod', ['clean:prod', 'devWithoutWatch', 'copy:prodViews', 'uglify:prod', 'cssmin:prod', 'daniel', 'includeSource:prod']);
+    grunt.registerTask('prod', ['devWithoutWatch', 'clean:prod', 'copy:prodViews', 'uglify:prod', 'cssmin:prod', 'setupVarsProd', 'includeSource:prod']);
 
-    grunt.registerTask("daniel", "", function () {
-        grunt.config("vendors", [''])
+    grunt.registerTask('setupVarsDev', '', function () {
+        var base = grunt.config('config').dev;
+
+        var vendors = grunt.file.readJSON("app/vendors.json");
+        var vendorsInLib = vendors.map(function (path) { return 'wwwroot/libs/' + path; });
+
+        var vendorsJs = vendorsInLib.filter(function (path) { return path.endsWith(".js"); });
+        var vendorsCss = vendorsInLib.filter(function (path) { return path.endsWith(".css"); });
+
+        grunt.config('vendors', vendors);
+        grunt.config('vendorsDevJs', vendorsJs);
+        grunt.config('vendorsDevCss', vendorsCss);
+
+        grunt.config('vendorsJs', vendorsJs);
+        grunt.config('vendorsCss', vendorsCss);
+        
+        grunt.config('appJs', [base + "/js/app.js", base + "/js/**/*.js"]);
+        grunt.config('appCss', [base + "/css/app.css"]);
+    });
+    grunt.registerTask('setupVarsProd', '', function () {
+        var base = grunt.config('config').prod;
+
+        grunt.config('vendorsJs', [base + '/vendors.min.js']);
+        grunt.config('vendorsCss', [base + '/vendors.min.css']);
+
+        grunt.config('appJs', [base + '/app.min.js']);
+        grunt.config('appCss', [base + '/app.min.css']);
     });
 };
